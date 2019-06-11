37b2c79aa6a40e2d810d68189a449273cab29c79
senya <kseniiakhromova@yandex.ru>
2019-06-11 15:32:48 +0300

    Initial commit

---
+++ ProxyWebApi/CacheKeys.cs
@@ -0,0 +1,7 @@
+namespace ProxyWebApi
+{
+    public class CacheKeys
+    {
+        
+    }
+}
---
+++ ProxyWebApi/Middleware/CacheMiddleware.cs
@@ -0,0 +1,7 @@
+namespace ProxyWebApi.Middleware
+{
+    public class CacheMiddleware
+    {
+        
+    }
+}
